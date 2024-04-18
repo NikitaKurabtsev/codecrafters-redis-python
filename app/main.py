356c0a1e7af4d01ee36e80267458f8bab312e474
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-18 16:02:53 +0200

    respond to multiple pings

--- app/main.py
+++ app/main.py
@@ -9,20 +9,13 @@ from .constants import (
 )
 
 
-def input_commands_handler(input_data: bytes) -> List[bytes]:
-    data = input_data.split(b" ")[2].split(b"\n")
-
-    return data
-
-
 def connection_handler(client_connection: socket.socket) -> None:
     input_data = client_connection.recv(MAX_BUFFER_SIZE)
-    data = input_commands_handler(input_data)
-
     response = b"+PONG\r\n"
 
-    for command in data:
-        client_connection.sendall(response)
+    while True:
+        if b"ping" in input_data:
+            client_connection.sendall(response)
 
 
 def main():
