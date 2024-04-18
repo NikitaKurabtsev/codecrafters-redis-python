480f8f640afff5f6f293dca024d3e851afa6faa5
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-18 15:57:41 +0200

    respond to multiple pings

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import socket
 from threading import Thread
+from typing import List
 
 from .constants import (
     SERVER_HOST,
@@ -8,19 +9,27 @@ from .constants import (
 )
 
 
-def connection_handeler(client_connection: socket.socket) -> None:
-    # data = client_connection.recv(MAX_BUFFER_SIZE)
+def input_commands_handler(input_data: bytes) -> List[bytes]:
+    data = input_data.split(b" ")[2].split(b"\n")
+
+    return data
+
+
+def connection_handler(client_connection: socket.socket) -> None:
+    input_data = client_connection.recv(MAX_BUFFER_SIZE)
+    data = input_commands_handler(input_data)
 
     response = b"+PONG\r\n"
 
-    client_connection.sendall(response)
+    for command in data:
+        client_connection.sendall(response)
 
 
 def main():
     server_socket = socket.create_server((SERVER_HOST, SERVER_PORT), reuse_port=True)
     while True:
         client_socket = server_socket.accept()[0]
-        thread = Thread(target=connection_handeler, args=[client_socket])
+        thread = Thread(target=connection_handler, args=[client_socket])
         thread.start()
 
 
