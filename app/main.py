8fe3b4b1c085ef1dfb67821105463ce454a434ef
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-18 15:23:00 +0200

    respond to ping

--- app/main.py
+++ app/main.py
@@ -1,14 +1,27 @@
 import socket
+from threading import Thread
 
 from .constants import (
     SERVER_HOST,
     SERVER_PORT,
+    MAX_BUFFER_SIZE,
 )
+
+
+def connection_handeler(client_connection: socket.socket) -> None:
+    # data = client_connection.recv(MAX_BUFFER_SIZE)
+
+    response = b"+PONG\r\n"
+
+    client_connection.sendall(response)
 
 
 def main():
     server_socket = socket.create_server((SERVER_HOST, SERVER_PORT), reuse_port=True)
-    server_socket.accept()
+    while True:
+        client_socket = server_socket.accept()[0]
+        thread = Thread(target=connection_handeler, args=[client_socket])
+        thread.start()
 
 
 if __name__ == "__main__":
