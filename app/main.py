35e6ee3055545098f11dbd8945c6166cab8ae1b0
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-19 10:30:39 +0200

    Implement the ECHO command

--- app/main.py
+++ app/main.py
@@ -10,17 +10,24 @@ from .constants import (
 
 PONG = b"+PONG\r\n"
 
+# TODO: Implement redis SimpleString Encoder
+
+def simple_string_encoder(string: bytes) -> bytes:
+    encoded_string = f"+ {string} \r\n".encode()
+    return encoded_string
+
 
 async def handle_client_connection(reader: StreamReader, writer: StreamWriter) -> None:
     while True:
         input_stream = await reader.read(MAX_BUFFER_SIZE)
         parsed_stream_data = input_stream.split(b"\r\n")[-2]
+        message = simple_string_encoder(parsed_stream_data)
         if not input_stream:
             break
         if parsed_stream_data == b"ping":
             writer.write(PONG)
         else:
-            writer.write(parsed_stream_data)
+            writer.write(message)
         await writer.drain()
     writer.close()
     await writer.wait_closed()
