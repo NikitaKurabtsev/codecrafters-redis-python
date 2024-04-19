ae784e572778c68152286bbd73e058a2d72a09f9
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-19 09:11:19 +0200

    Implement the ECHO command

--- app/main.py
+++ app/main.py
@@ -14,14 +14,13 @@ PONG = b"+PONG\r\n"
 async def handle_client_connection(reader: StreamReader, writer: StreamWriter) -> None:
     while True:
         input_stream = await reader.read(MAX_BUFFER_SIZE)
-        print(input_stream)
+        parsed_stream_data = input_stream.split(b"\r\n")[-2]
         if not input_stream:
             break
-        if input_stream == b"ping":
+        if parsed_stream_data == b"ping":
             writer.write(PONG)
-        # else:
-        #     input_stream_encoded = (b"+" + input_stream + b"\r\n")
-        #     writer.write(input_stream_encoded)
+        else:
+            writer.write(parsed_stream_data)
         await writer.drain()
     writer.close()
     await writer.wait_closed()
