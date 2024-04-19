52b6e0e8df8e7d5adf969f514a23c6537a87c972
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-19 10:34:31 +0200

    Implement the ECHO command

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ PONG = b"+PONG\r\n"
 # TODO: Implement redis SimpleString Encoder
 
 def simple_string_encoder(string: bytes) -> bytes:
-    encoded_string = f"+{string}\r\n".encode()
+    encoded_string = b"+" + string + b"\r\n"
     return encoded_string
 
 
