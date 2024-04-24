a9ff6a877e249d8efa089c7434bb8a996879e8e2
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-24 16:17:50 +0200

    7 stage

--- app/main.py
+++ app/main.py
@@ -40,6 +40,7 @@ async def handle_client_connection(reader: StreamReader, writer: StreamWriter) -
             writer.write(encoded_message)
 
         elif command == b"get":
+            key = input_stream.split(b"\r\n")[-1]
             record = database_manager.fetch_record_by_key(key)
             print(record)
             encoded_message = simple_string_encoder(record.value)
