be9ea918e6b5b001c769575d23dc2520f61db34b
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-24 16:12:52 +0200

    7 stage

--- app/parser.py
+++ app/parser.py
@@ -11,7 +11,7 @@ class InputStreamParser:
     @staticmethod
     def parse_key_value(input_stream: bytes) -> Tuple[bytes, bytes]:
         """Parses the key and value from the input stream."""
-        key = input_stream.split(b"\r\n")[-4]
-        value = input_stream.split(b"\r\n")[-2]
+        key = input_stream.split(b"\r\n")[4]
+        value = input_stream.split(b"\r\n")[6]
 
         return key, value
